ee4e3d670143ecfc105b03a38c5b6d3bf997e107
chupanw <chupanw@cs.cmu.edu>
2015-06-08 11:51:42 +0800

    Added test cases

--- test/edu/cmu/nway/SimpleTests.java
+++ test/edu/cmu/nway/SimpleTests.java
@@ -47,4 +47,25 @@ public class SimpleTests {
             e.printStackTrace();
         }
     }
+
+
+    @Test
+    public void test2() {
+        File baseFile = new File("testfiles/patch/test2/Base.java");
+        File[] patchFiles = new File[3];
+        patchFiles[0] = new File("testfiles/patch/test2/Patch1.java");
+        patchFiles[1] = new File("testfiles/patch/test2/Patch2.java");
+        patchFiles[2] = new File("testfiles/patch/test2/Patch3.java");
+        NWayMerge merger = new NWayMerge(baseFile, patchFiles);
+        try {
+            merger.merge();
+            String solContent = Utility.readContentFromFile(new File("testfiles/patch/test2/solution"));
+            String testContent = Utility.readContentFromFile(new File("testOutput"));
+            Assert.assertEquals(solContent, testContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
